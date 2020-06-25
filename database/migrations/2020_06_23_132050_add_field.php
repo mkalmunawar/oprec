9b7a05e6461185b302f83c86f7610afa309a0aaf
MKAlmunawar <karomahsmp3pdl@gmail.com>
2020-06-25 19:51:46 +0700

    initial commit

---
+++ database/migrations/2020_06_23_132050_add_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('email');
+            $table->string('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
+    }
+}
